2d7feba992adc21de18e8431c4dfc1839f95d543
idosinchuk <idosinchuk@host.docker.internal>
2019-09-12 15:22:56 +0200

    Refactored for standart.

--- src/main/java/com/idosinchuk/handlemovies/discovery/EurekaServerApplication.java
+++ src/main/java/com/idosinchuk/handlemovies/discovery/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.idosinchuk.eureka;
+package com.idosinchuk.handlemovies.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
